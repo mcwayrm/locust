f8538281b440fceb3faf5de637f7c04bd0754294
Ryan McWay <ryan.mcway@gmail.com>
2023-02-07 21:59:45 -0600

    Minor cleaning of the locust swarm data

--- 01_clean/01_test_clean_fao_swarms_df.R
+++ 01_clean/01_test_clean_fao_swarms_df.R
@@ -43,29 +43,48 @@ library(tmap)
 #######################################################
 
 # Bring in raw adult locust data
-sf_locust <- sf::read_sf("./data/raw/fao_locust_hub/fao_adults_sf/")
+sf_locust <- sf::read_sf("./data/raw/fao_locust_hub/fao_swarms_sf/")
+    # Check CRS
+    st_crs(sf_locust) # "EPSG",4326
+
+# TODO: Consider if any of the other FAO information is useful or needed. Or if we should only be focusing on the swarms (or adult population). 
+# TODO: Perhaps hoppers (which are not swarms) could be used as an IV. And the ecological data needs to be processed as a potential IV measure as well. 
+
+# List of countries that need to be covered from GDAM data (given there is reliable data)
+sort(table(subset(sf_locust$COUNTRYID, sf_locust$REPRELIAB == 1)), decreasing = T)
+    # NOTE: Only 277 observations were unreliable (0.6% of sample)
 
 # Convert to data.table for cleaning
+df_locust <- setDT(sf_locust)
 
 #######################################################
 # 02: Clean Data
 #######################################################
 
 # Only keep needed variables 
-sf_locust[, c("CAT","") := NULL]
+    # TODO: Drop all but bare minimum. Then add things back in as they become useful. 
+df_locust <- df_locust[, c("OBJECTID", "STARTDATE", "FINISHDATE", "EXACTDATE", "LOCNAME", 
+                           "COUNTRYID", "LOCUSTID", "REPORTID", "REPRELIAB", "LOCPRESENT", 
+                           "AREAHA", "CONFIRMATN", "GADFLYFROM", "GADFLYTO", "geometry")]
 
 # Rename variable names
 
 # Remove problematic observations
+    # Drop unreliable observations 
+    df_locust <- df_locust[REPRELIAB == 1,]
 
 # Create generally useful information (dummies)
 
+    # TODO: Create variable for Year, Month, Date
 
 #######################################################
 # 03: Save Cleaned Data
 #######################################################
 
 # Restore as sf objection prior to export. (use default projection from raw data)
-
+sf_locust <- st_as_sf(df_locust)
+st_crs(sf_locust) <- 4326
+    
 # Save 
 st_write(sf_locust,"./data/clean/locust/sf_locust.gdb" )
+    # TODO: Issue with writing out the GBD
